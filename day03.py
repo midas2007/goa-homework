ec35c402a4f7414fbe90079787ef5decbb3e9e72
midas2007 <giorexvo007@gmail.com>
2024-03-08 14:40:11 +0400

    first commit

---
+++ day03.py
@@ -0,0 +1,21 @@
+my_name = "demuri"
+my_surname = "rexviashvili"
+print(my_name,my_surname)
+my_name = "nato"
+my_surname ="kacelashvili"
+print(my_name,my_surname)
+my_name = "giorgi"
+my_surname = "rexviashvili"
+print(my_name,my_surname)
+my_name = "nika"
+my_surname = "rexviashvili"
+print(my_name,my_surname)
+my_name = "dato"
+my_surname = "rexviashvili"
+print(my_name,my_surname)
+my_name = "luka"
+my_surname = "rexviashvili"
+print(my_name,my_surname)
+my_name = "dachi"
+my_surname = "rexviashvili"
+print(my_name,my_surname)
